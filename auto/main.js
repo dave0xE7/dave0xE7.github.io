5ee81bfaed8e0ac04767ccf959e90d812a420956
dave0xE7 <redcorwp@gmail.com>
2022-08-21 01:32:02 +0200

    fix

--- auto/main.js
+++ auto/main.js
@@ -1,5 +1,6 @@
 
 var ua;
+var is_android;
 var is_native_android;
 var is_linux;
 
@@ -11,10 +12,12 @@ window.onload = function () {
 
 
     ua = navigator.userAgent;
+    is_android = ((ua.indexOf('Mozilla/5.0') > -1 && ua.indexOf('Android') > -1));
     is_native_android = ((ua.indexOf('Mozilla/5.0') > -1 && ua.indexOf('Android ') > -1 && ua.indexOf('AppleWebKit') > -1) && (ua.indexOf('Version') > -1));
     is_linux = (ua.indexOf('Linux') > -1);
 
     document.getElementById('useragent').innerText=navigator.userAgent;
+    document.getElementById('is_android').innerText="is_android="+is_android;
     document.getElementById('is_native_android').innerText="is_native_android="+is_native_android;
     document.getElementById('is_linux').innerHTML="is_linux="+is_linux;
 
